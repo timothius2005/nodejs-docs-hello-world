502df798e29bf4746bd6a03f59a22c207c9501e0
Tim Woodson <timtohy.woodosn@outlook.com>
2020-07-04 22:58:36 -0700

    updated response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Woodson & Co.");
+    response.end("Woodson & Co. Established 2020");
 });
 
 const port = process.env.PORT || 1338;
