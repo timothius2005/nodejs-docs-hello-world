d7232c591a6150a03d9df1e4a9498849441330c3
Tim Woodson <timtohy.woodosn@outlook.com>
2020-07-04 22:45:41 -0700

    a working app

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Woodson & Co.");
 });
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 1338;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
