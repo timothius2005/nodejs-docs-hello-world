621e6bd7c4ace5a686b6abf70d16993a994ae0fa
Tim Woodson <timtohy.woodosn@outlook.com>
2020-07-11 20:01:58 -0700

    point package to client

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Root INdex.js file");
+    response.end("Root Index.js file");
 });
 
 console.log('another line of text.')
